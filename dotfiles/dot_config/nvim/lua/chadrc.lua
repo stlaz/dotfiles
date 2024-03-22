6a481ea917452dcd60040b9a1d2632fac1a74c4a
Stanislav Laznicka <slaznick@redhat.com>
2024-03-22 17:10:56 +0100

    add neovim config

---
+++ dotfiles/dot_config/nvim/lua/chadrc.lua
@@ -0,0 +1,15 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "one_light",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+require("custom")
+
+return M
