309db58e2dad8d5bc6c258a3e3d99ba2988596b5
Stanislav Láznička <slaz@seznam.cz>
2024-09-23 09:13:21 +0200

    nvim: kickstart update

--- dotfiles/dot_config/nvim/lua/kickstart/plugins/indent_line.lua
+++ dotfiles/dot_config/nvim/lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,12 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '▏',
+        tab_char = '├',
+      },
+      scope = { show_start = false, show_end = false },
+    },
   },
 }
